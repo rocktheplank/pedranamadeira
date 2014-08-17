390e28c30a55df1a2f66afc4ab3526450f63266e
pcdro <pcdro_torres@hotmail.com>
2014-08-17 20:18:34 -0300

    Criacao da classe "HelloBro"

---
+++ workspace_RTP/HelloBro/src/com/rocktheplank/intro/HelloBro.java
@@ -0,0 +1,10 @@
+package com.rocktheplank.intro;
+
+public class HelloBro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello CHAMPZ");
+	}
+
+}
