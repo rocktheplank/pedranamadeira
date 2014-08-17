03eab25c651ff73fa7372c8a84ea04340abaa9b5
pcdro <pcdro_torres@hotmail.com>
2014-08-17 20:21:19 -0300

    Ajuste de identacao e limpeza de codigo da classe "Hello Bro"

--- workspace_RTP/HelloBro/src/com/rocktheplank/intro/HelloBro.java
+++ workspace_RTP/HelloBro/src/com/rocktheplank/intro/HelloBro.java
@@ -3,8 +3,6 @@ package com.rocktheplank.intro;
 public class HelloBro {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello Bro");
 	}
-
 }
